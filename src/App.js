ef5dddf55377d95e26f845b8867e74b56ce2fc19
tamik <t.tomaev2016@yandex.ru>
2021-06-22 18:45:16 +0300

    added index.html file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import HeaderContainer from './components/Header/HeaderContainer';
+import NavbarContainer from './components/Navbar/NavbarContainer';
+import Content from './components/Content/Content';
+import {loginProcess} from './redux/reducers/appReducer';
+import {connect} from 'react-redux';
+import {compose} from 'redux';
+import Preloader from './components/common/Preloader/Preloader';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount() {
+	  this.props.loginProcess();
+  }
+  
+  
+  render() {
+	  if(!this.props.initialized) {
+		  <Preloader />
+	  }
+	  return (
+		<div>
+			<HeaderContainer />
+			<div className='wrapper'>
+				<NavbarContainer />
+				<Content />
+			</div>
+		</div>
+	  );
+  }
 }
 
-export default App;
+let mapStateToProps = (state)=> {
+	return {
+		initialized: state.app.initialized
+	}
+}
+
+export default compose(
+	connect(mapStateToProps, {loginProcess})
+)(App);
